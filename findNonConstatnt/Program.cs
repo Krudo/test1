12b59de309d9616a93ab61e69f488976f4563882
Krudo <dev@krudo.net>
2015-10-19 22:08:29 +0300

    start

---
+++ findNonConstatnt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findNonConstatnt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lkjlkj");
+            Console.ReadKey();
+        }
+    }
+}
